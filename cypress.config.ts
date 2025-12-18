56fb904b34ec31cc5d137e620f02cd59afb02afb
Oliwier Przewlocki <oprzewlocki@student.tgm.ac.at>
2025-12-18 11:04:00 +0100

    add cicd tools research, fix cors issues in tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter', // HTML Reports
+  video: true, // Record video of runs
   e2e: {
+    baseUrl: 'http://localhost:5173', // Frontend URL
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
+    defaultCommandTimeout: 6000,
   },
 });
